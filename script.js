00380156fafacffa840d72c1947f02dee3ef367d
BeAsT436 <yuriryabenko436@gmail.com>
2023-10-24 10:08:27 +0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,49 +1,61 @@
-// Оголосити змінні:
-    let seconds = 0
-   const clearButton = document.querySelector("#clearButton")
-   const stopButton = document.querySelector("#stopButton")
-   const startButton = document.querySelector("#startButton")
-   let timer 
+function Timer() {
+  const clearButton = document.querySelector("#clearButton");
+  const stopButton = document.querySelector("#stopButton");
+  const startButton = document.querySelector("#startButton");
+  const timerEl = document.querySelector("#timer");
+  let timer;
+  let seconds = 0;
+  let minutes = 0;
+  let milliseconds = 0;
 
-function updateTimer(s){
-    seconds++
-    document.querySelector("#timer").innerText = seconds
+  function updateTimer() {
+    milliseconds++;
+
+    if (milliseconds === 1000) {
+      milliseconds = 0;
+      seconds++;
+      if (seconds === 60) {
+        seconds = 0;
+        minutes++;
+      }
+    }
+
+    timerEl.textContent = `${addZero(minutes)}:${addZero(seconds)}:${addZero(
+      milliseconds,
+      3
+    )}`;
+  }
+  function addZero(value, length = 2) {
+    return String(value).padStart(length, "0");
+  }
+
+  function startTimer() {
+    timer = setInterval(updateTimer, 1);
+  }
+
+  function stopTimer() {
+    clearInterval(timer);
+  }
+
+  function clearTimer() {
+    clearInterval(timer);
+    milliseconds = 0;
+    seconds = 0;
+    minutes = 0;
+    timerEl.textContent = "00:00:000";
+  }
+
+  startButton.addEventListener("click", () => {
+    startTimer();
+  });
+
+  stopButton.addEventListener("click", () => {
+    stopTimer();
+  });
+
+  clearButton.addEventListener("click", () => {
+    clearTimer();
+  });
 }
-   
 
-function startTimer(s){
-    setInterval(updateTimer, 1000) 
-}
-
-function stopTimer(s){
-    clearInterval(timer)
-    console.log(timer);
-}
-   
-
-function clearTimer(s){
-    clearInterval(timer) 
-    seconds = 0
-    document.querySelector("#timer").innerText = seconds
-}
-   
-
-// Коли кнопка "Start" натиснута:
-//    Викликаємо startTimer()
-startButton.addEventListener("click",()=>{
-    startTimer()
-})
-
-// Коли кнопка "Stop" натиснута:
-//    Викликаємо stopTimer()
-stopButton.addEventListener("click",()=>{
-    stopTimer()
-    console.log("no");
-
-})
-
-// Коли кнопка "Clear" натиснута:
-//    Викликаємо clearTimer()
-clearButton.addEventListener("click",()=>{
-    clearTimer()
-})
+Timer();
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "stop-watch-cache-v1"
+const URLS_TO_CACHE = [
+    './stopwatch/',
+    './stopwatch/index.html',
+    './stopwatch/icon.png',
+    './stopwatch/script.js',
+    './stopwatch/styles.css',
+]
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(URLS_TO_CACHE).catch((error) => {
+          console.error('Cache addAll error:', error)
+        })
+      })
+    )
+  })
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return (
+          response ||
+          fetch(event.request).catch((error) => {
+            console.error('Fetch error:', error)
+          })
+        )
+      })
+    )
+  }) 
